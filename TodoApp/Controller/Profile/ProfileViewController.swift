cd879d2c58d4d129ab9cc35c480ab933a434ec4b
Serhii Liubeznov <moltuc@gmail.com>
2023-03-07 20:54:10 +0200

    update profile controller, add profile view model

--- TodoApp/Controller/Profile/ProfileViewController.swift
+++ TodoApp/Controller/Profile/ProfileViewController.swift
@@ -6,20 +6,48 @@
 //
 
 import UIKit
+import Combine
 
 class ProfileViewController: BaseViewController {
     
+    @IBOutlet private var emailLabel: UILabel!
+    @IBOutlet private var nameLabel: UILabel!
+    
     var coordintator: ((MainCoordinator.Destination) -> Void)?
+    
+    private let viewModel = ProfileViewModel()
+    private var cancellableSet: Set<AnyCancellable> = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupSiblings()
     }
     
     // MARK: - Actions
     @IBAction func logoutButtonPressed() {
         coordintator?(.logout)
+    }
+}
+
+// MARK: - Combine
+extension ProfileViewController {
+    
+    private func setupSiblings() {
+        
+        viewModel.$email
+            .sink { [weak self] email in
+                self?.emailLabel.text = email
+            }
+            .store(in: &cancellableSet)
+        
+        viewModel.$name
+            .sink { [weak self] name in
+                self?.nameLabel.text = name
+            }
+            .store(in: &cancellableSet)
+        
+        viewModel.update()
     }
 }
 
---
+++ TodoApp/ViewModel/Profile/ProfileViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewModel.swift
+//  TodoApp
+//
+//  Created by Serhii Liubeznov on 07.03.2023.
+//
+
+import UIKit
+
+class ProfileViewModel {
+    
+    @Published var name = ""
+    @Published var email = ""
+    
+    private let dataService:UserDataServiceProtocol
+    
+    init() {
+        dataService = UserDataService()
+    }
+    
+    func update() {
+        if let user = dataService.getUser() {
+            self.name = user.fullName()
+            self.email = user.email
+        }
+    }
+}
